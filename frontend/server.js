22a6d6f286293961a3f67b6ce56adb35d57318d3
Rohit Patil <rohitvpatil04@gmail.com>
2025-06-28 18:50:50 +0530

    Add files via upload

---
+++ frontend/server.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.static(__dirname)); // Serve index.html and other static files
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+});
+
+app.get("/success.html", (req, res) => {
+    res.sendFile(path.join(__dirname, "success.html"));
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Frontend running at http://0.0.0.0:${PORT}`);
+});
